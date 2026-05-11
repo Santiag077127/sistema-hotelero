92f650a6198cc3b1933e1da709575c83cca5f1fc
Santiag077127 <santiagogordoperez77@gmail.com>
2026-05-11 08:08:18 -0500

    feact:(The service delivery domain is added).

--- 05_rollbacks/01_ddl/03_tables/04_create_billing_domain_tables.sql
+++
@@ -1,5 +0,0 @@
--- Drop billing domain
-DROP TABLE IF EXISTS invoice_detail CASCADE;
-DROP TABLE IF EXISTS partial_payment CASCADE;
-DROP TABLE IF EXISTS invoice CASCADE;
-DROP TABLE IF EXISTS proforma_invoice CASCADE;
